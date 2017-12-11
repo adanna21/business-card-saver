3256a422725994fc44518cac46afc2b308868686
Adanna Omatu <adaomatu@gmail.com>
2017-12-11 14:44:55 -0500

    added items to contact migration

--- db/migrate/20171211193617_create_contacts.rb
+++ db/migrate/20171211193617_create_contacts.rb
@@ -1,6 +1,14 @@
 class CreateContacts < ActiveRecord::Migration[5.1]
   def change
     create_table :contacts do |t|
+      t.string :name
+      t.string :company
+      t.string :department
+      t.string :title
+      t.string :email
+      t.string :phone
+      t.text :description
+      t.belongs_to :user, index: true
 
       t.timestamps
     end

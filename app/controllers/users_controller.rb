7465bd3f2b621577b96ab24bad4da9da1d025e0f
Adanna Omatu <adaomatu@gmail.com>
2017-12-11 19:05:42 -0500

    added new and create method in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+    before_action :ensure_signed_out, only: [:new, :create]
+    before_action :ensure_signed_in, only: [:show, :index]
+
+    def new
+        @user = User.new
+    end
+
+    def create 
+        @user = User.new(create_params)
+
+        if @user.save
+            sign_in(@user)
+            flash[:notice] = 'Congrats! You are signed in!'
+            redirect_to users_path
+        else
+            flash[:error] = @user.errors.full_messages.join(', ')
+            render :new
+        end
+    end
+    
 end

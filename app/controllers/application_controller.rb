dea46ca98810edd185236937e47e396fd1c6735d
Adanna Omatu <adaomatu@gmail.com>
2017-12-11 19:53:47 -0500

    added sign in, sign out, current user, and find current user methods to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  help_method :current_user
+
+  private
+
+  def sign_in(user)
+    session[:session_token] = SecureRandom.urlsafe_base64
+    user.update!(session_token: session[:session_token])
+  end
+
+  def sign_out
+    return unless current_user
+    current_user.update!(session_token: nil)
+    session.delete(:session_token)
+  end
+
+  def current_user
+    @current_user ||= find_current_user
+  end
+
+  def find_current_user
+    session_token = session[:session_token]
+    session_token && User.find_by(session_token: session_token)
+  end
 end
